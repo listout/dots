1eccc73ab3f953ff63406183a7235513669e32f8
listout <listout@protonmail.com>
2021-07-10 10:18:17 +0530

    triangular prompt, previously lambda and delta

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,5 +1,5 @@
 fpath+=$HOME/.config/zsh/pure
 autoload -U promptinit; promptinit
-PURE_PROMPT_SYMBOL=λ
-PURE_PROMPT_VICMD_SYMBOL=ẟ
+PURE_PROMPT_SYMBOL=▲ #λ
+PURE_PROMPT_VICMD_SYMBOL=▼ #ẟ
 prompt pure
