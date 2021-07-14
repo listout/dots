d127e4633f9d2334de8414e9bc307dce61882f9d
listout <listout@protonmail.com>
2021-07-14 23:47:12 +0530

    added options for pure prompt and some asthetics

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,5 +1,13 @@
 fpath+=$HOME/.config/zsh/pure
+
+# prompt init
 autoload -U promptinit; promptinit
+
+# prompt symbols
 PURE_PROMPT_SYMBOL=▲ #λ
 PURE_PROMPT_VICMD_SYMBOL=▼ #ẟ
+
+# turn on git stash status
+zstyle :prompt:pure:git:stash show yes
+
 prompt pure
