e20c8e51c9ac6168152881c268fad23960c23e23
listout <listout@protonmail.com>
2022-01-17 23:18:23 +0530

    use environment variable for temperature module

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+for i in /sys/class/hwmon/hwmon*/temp*_input; do
+	if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "k10temp: Tctl" ]; then
+		export HWMON_PATH="$i"
+	fi
+done
 killall -q polybar
 echo "---" | tee -a /tmp/polybar1.log
 polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
 
- echo "Bar launched"
+echo "Bar launched"
