3e37a565f516f57b56249f5e0051978d4c2173af
listout <listout@protonmail.com>
2022-01-30 21:41:10 +0530

    Get the wifi interface name from environment variable, from launch script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,6 +5,7 @@ for i in /sys/class/hwmon/hwmon*/temp*_input; do
 		export HWMON_PATH="$i"
 	fi
 done
+export DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
 killall -q polybar
 echo "---" | tee -a /tmp/polybar1.log
 polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
