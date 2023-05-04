0f0f9a21cc7e13acc2470fa7290e65de7059bb66
listout <listout@protonmail.com>
2023-05-04 11:23:46 +0530

    zsh: aliases: dont alias for multiple verions of ESP-IDF
    Signed-off-by: listout <listout@protonmail.com>

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -17,12 +17,6 @@ then
 	export MENUCONFIG_STYLE=monochrome
 	export IDF_CCACHE_ENABLE=1
 fi
-if [ -d "$HOME/esp/esp-idf-v5.0" ]
-then
-	alias get_idf_5='. $HOME/esp/esp-idf-v5.0/export.sh'
-	export MENUCONFIG_STYLE=monochrome
-	export IDF_CCACHE_ENABLE=1
-fi
 
 export LESS=-R
 export LESS_TERMCAP_mb=$'\E[1;31m'     # begin blink
