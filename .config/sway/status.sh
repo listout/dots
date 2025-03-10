95d32690655a4393c59df96aa2fda2db45c22f15
Brahmajit Das <brahmajit.xyz@gmail.com>
2025-03-10 20:33:03 +0530

    sway: status.sh: dont use ping to avoid failure during no internet
    Signed-off-by: Brahmajit Das <brahmajit.xyz@gmail.com>

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -26,8 +26,8 @@ awk '{print $2}')
 ip_addr=$(ip route get 1.1.1.1 | head -1 | cut -d' ' -f7)
 network=$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')
 # interface_easyname grabs the "old" interface name before systemd renamed it
-interface_easyname=$(dmesg | grep $network | grep renamed | awk 'NF>1{print $NF}')
-ping=$(ping -c 1 www.google.es | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
+#interface_easyname=$(dmesg | grep $network | grep renamed | awk 'NF>1{print $NF}')
+#ping=$(ping -c 1 www.google.es | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
 
 # CPU temp
 cpu_temp=$(sensors | grep -oP 'CPU.*?\+\K[0-9.]+')
